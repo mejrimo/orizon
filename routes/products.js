e678b302489f3f4fdf7da2c04ec8e18fdbf578d0
mejrimo <mejrimo.dev@gmail.com>
2023-08-10 14:29:55 +0200

    product routes created and configured

---
+++ routes/products.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+
+// IMPORT PRODUCT MODEL
+const Product = require('../models/product');
+
+// CUSTOM MIDDLEWARE TO SEARCH FOR THE PRODUCT
+const getProduct = require('../middlewares/getProduct');
+
+// BUILT-IN MIDDLEWARE TO MANAGE JSON FILES
+router.use(express.json());
+
+//GET ALL PRODUCTS
+router.get('/', async (req, res) => {
+	try {
+		const products = await Product.find();
+		res.json(products);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+// GET ONE PRODUCT
+router.get('/:id', getProduct, (req, res) => {
+	res.json(res.product);
+});
+
+// CREATE A NEW PRODUCT
+router.post('/', async (req, res) => {
+	const product = new Product({
+		name: req.body.name,
+	});
+
+	try {
+		const newProduct = await product.save();
+		res.status(201).json(newProduct);
+	} catch (err) {
+		res.status(400).json({ message: err.message });
+	}
+});
+
+// UPDATE A PRODUCT
+router.patch('/:id', getProduct, async (req, res) => {
+	const { name } = req.body;
+	if (name != null) {
+		res.product.name = name;
+	}
+
+	try {
+		const updatedProduct = await res.product.save();
+		res.json(updatedProduct);
+	} catch (err) {
+		res.status(400).json({ message: err.message });
+	}
+});
+
+// DELETE A PRODUCT
+router.delete('/:id', getProduct, async (req, res) => {
+	try {
+		await res.product.deleteOne({ _id: req.params.id });
+		res.json({ message: 'Deleted Product' });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+module.exports = router;
