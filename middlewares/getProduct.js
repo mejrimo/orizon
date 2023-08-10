49ea5726608ed515930eb7bf5bb133ce978e078f
mejrimo <mejrimo.dev@gmail.com>
2023-08-10 14:29:04 +0200

    custom function to get a product through the id

---
+++ middlewares/getProduct.js
@@ -0,0 +1,19 @@
+const Product = require('../models/product');
+
+// FUNCTION TO FIND A SINGLE PRODUCT
+async function getProduct(req, res, next) {
+	let product;
+	try {
+		product = await Product.findById(req.params.id);
+		if (!product) {
+			return res.status(404).json({ message: 'Cannot find the product' });
+		}
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
+
+	res.product = product;
+	next();
+}
+
+module.exports = getProduct;
