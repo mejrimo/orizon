817e38ff2051975020119b155e5d0ffbca889fd7
mejrimo <mejrimo.dev@gmail.com>
2023-08-11 20:48:10 +0200

    user model configured

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// CREATE A SCHEMA SO THAT EVERY ELEMENT IN THE DB WILL HAVE THE INFORMATION YOU NEED
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	surname: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+});
+
+module.exports = mongoose.model('User', userSchema);
