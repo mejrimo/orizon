acb0e92879f6fa4cb5d357343e3eff7da6076af5
mejrimo <mejrimo.dev@gmail.com>
2023-08-13 21:01:34 +0200

    name/surname/email validation implemented

--- models/user.js
+++ models/user.js
@@ -1,21 +1,31 @@
 const mongoose = require('mongoose');
 
+// LIBRARY TO ADD UNIQUE VALIDATION
+const uniqueValidator = require('mongoose-unique-validator');
+
 // CREATE A SCHEMA SO THAT EVERY ELEMENT IN THE DB WILL HAVE THE INFORMATION YOU NEED
 const userSchema = new mongoose.Schema({
 	name: {
 		type: String,
-		required: true,
+		required: [true, "can't be blank"],
+		match: [/^[a-zA-Z\sÀ-ÿ']+$/, 'is invalid'],
 	},
 
 	surname: {
 		type: String,
-		required: true,
+		required: [true, "can't be blank"],
+		match: [/^[a-zA-Z\sÀ-ÿ']+$/, 'is invalid'],
 	},
 
 	email: {
 		type: String,
-		required: true,
+		lowercase: true,
+		unique: true,
+		required: [true, "can't be blank"],
+		match: [/\S+@\s+\.\S+/, 'is invalid'],
 	},
 });
+
+userSchema.plugin(uniqueValidator, { message: 'already taken' });
 
 module.exports = mongoose.model('User', userSchema);
