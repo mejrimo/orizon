963b0ee191c37c4352d6d1eed877e300313444c5
mejrimo <mejrimo.dev@gmail.com>
2023-08-14 20:10:39 +0200

    logic modified

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology
 // HANDLE ERROR/SUCCESS CONNECTING TO DATABASE
 const db = mongoose.connection;
 db.on('error', (err) => console.error(err));
-db.once('open', () => console.log('Connected to Database'));
+db.once('open', () => {
+	console.log('Connected to Database');
+	// TELL THE APP TO LISTEN TO A PORT (CORRECTION: START THE SERVER ONLY AFTER THE CONNECTION TO DB)
+	app.listen(port, () => console.log(`Server started on localhost:${port}!`));
+});
 
 // IMPORT ROUTES USED BY THE APP
 const productsRouter = require('./routes/products');
@@ -23,6 +27,3 @@ const ordersRouter = require('./routes/orders');
 app.use('/api/products', productsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/orders', ordersRouter);
-
-// TELL THE APP TO LISTEN TO A PORT
-app.listen(port, () => console.log(`Server started on localhost:${port}!`));
